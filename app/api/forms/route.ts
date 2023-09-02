74290772808a3377b1d75564a23302ea357e8849
Lindrit <lindritsulaj2.0@gmail.com>
2023-09-02 23:25:47 +0200

    `api/forms` only same-origin

--- app/api/forms/route.ts
+++ app/api/forms/route.ts
@@ -3,6 +3,9 @@ import { NextResponse } from "next/server";
 export async function GET() {
   return NextResponse.json({ title: 'Hello Next.js', forms: [1, 2, 3, 4, 5, 6] }, {
     status: 200,
-    statusText: 'Success'
+    statusText: 'Success',
+    headers: {
+      'Access-Control-Allow-Origin': process.env.NEXTAUTH_URL as string
+    }
   })
 }
