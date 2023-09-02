d015b0794e6b985cc6b2400427317a7e844b3423
Lindrit <lindritsulaj2.0@gmail.com>
2023-09-02 23:39:51 +0200

    Learning Routes

--- app/api/forms/route.ts
+++ app/api/forms/route.ts
@@ -1,11 +1,15 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET() {
-  return NextResponse.json({ title: 'Hello Next.js', forms: [1, 2, 3, 4, 5, 6] }, {
+export async function GET(request: NextRequest) {
+
+  // request.credentials "same-origin"
+
+  return NextResponse.json({ title: 'Hello Next.js', forms: [1, 2, 3, 4, 5, 6], headers: request.credentials }, {
     status: 200,
     statusText: 'Success',
     headers: {
-      'Access-Control-Allow-Origin': process.env.NEXTAUTH_URL as string
+      'Access-Control-Allow-Origin': process.env.NEXTAUTH_URL as string,
+      'Access-Control-Allow-Methods': ''
     }
   })
 }
--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 import prisma from '@/lib/prisma'
 
 async function getForms() {
-  const res = await fetch(`${process.env.NEXTAUTH_URL}/api/forms/`)
+  const res = await fetch(`${process.env.NEXTAUTH_URL}/api/forms/`, { cache: 'no-cache'})
   return res.json();
 }
 
