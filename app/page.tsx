a9226aad1d98bf7406916f9331ef650cb23be11f
Lindrit <lindritsulaj2.0@gmail.com>
2023-09-02 21:06:05 +0200

    hi

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,18 @@
 import Image from 'next/image'
 import prisma from '@/lib/prisma'
 
+async function getForms() {
+  const res = await fetch(`${process.env.NEXTAUTH_URL}api/forms/`)
+  return res.json();
+}
+
 export default async function Home() {
+  const forms = await getForms();
   return (
     <main>
-      Hello World
+      <pre>
+        {JSON.stringify(forms, null, 2)}
+      </pre>
     </main>
   )
 }
--- components/AuthProvider.tsx
+++ components/AuthProvider.tsx
@@ -3,7 +3,6 @@
 import React from 'react'
 import { SessionProvider } from 'next-auth/react'
 
-
 export default function AuthProvider({ children }: { children: React.ReactNode }) {
   return (
     <SessionProvider>
