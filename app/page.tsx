9af62c0bcf1e88b5867659c046a9da086a3da454
Lindrit <lindritsulaj2.0@gmail.com>
2023-09-02 21:09:22 +0200

    ...

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 import prisma from '@/lib/prisma'
 
 async function getForms() {
-  const res = await fetch(`${process.env.NEXTAUTH_URL}api/forms/`)
+  const res = await fetch(`${process.env.NEXTAUTH_URL}/api/forms/`)
   return res.json();
 }
 
