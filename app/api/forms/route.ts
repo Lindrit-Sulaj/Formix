4a316e5f712cfc8b90263186b9b5c50def1e6288
Lindrit <lindritsulaj2.0@gmail.com>
2023-09-04 17:53:39 +0200

    cors

--- app/api/forms/route.ts
+++ app/api/forms/route.ts
@@ -4,8 +4,12 @@ export async function GET(request: NextRequest) {
 
   // request.credentials "same-origin"
 
-  return NextResponse.json({ title: 'Hello Next.js', forms: [1, 2, 3, 4, 5, 6], headers: request.credentials }, {
+  return NextResponse.json({ title: 'Hello Next.js', forms: [1, 2, 3, 4, 5, 6], url: request.nextUrl, other: request.mode  }, {
     status: 200,
     statusText: 'Success',
+    headers: {
+      'Access-Control-Allow-Origin': process.env.NEXTAUTH_URL as string,
+      'Access-Controll-Allow-Methods': 'GET'
+    }
   })
 }
