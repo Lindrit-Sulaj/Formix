10116a56325a17b00bc6f19e09507cbff5497c64
Lindrit <lindritsulaj2.0@gmail.com>
2023-09-04 14:09:57 +0200

    removed headers from `api/forms`

--- app/api/forms/route.ts
+++ app/api/forms/route.ts
@@ -7,9 +7,5 @@ export async function GET(request: NextRequest) {
   return NextResponse.json({ title: 'Hello Next.js', forms: [1, 2, 3, 4, 5, 6], headers: request.credentials }, {
     status: 200,
     statusText: 'Success',
-    headers: {
-      'Access-Control-Allow-Origin': process.env.NEXTAUTH_URL as string,
-      'Access-Control-Allow-Methods': ''
-    }
   })
 }
